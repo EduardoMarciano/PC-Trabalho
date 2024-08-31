d6bcaceaf3a2d166488e45810e4da911fea60a82
Eduardo Marciano <89046882+EduardoMarciano@users.noreply.github.com>
2024-08-30 21:11:53 -0300

    feat: adding worker logic

--- boliche.cpp
+++ boliche.cpp
@@ -6,12 +6,15 @@
 #include <unistd.h>
 #include <semaphore.h>
 
-#define NumeroPistas 5
+#define NumeroPistas 3
 #define NumeroCadeiras 5
 #define NumeroClientes 20
 
+//Variáveis
+int pistasParaLevantarPinos[NumeroPistas] = {0};
+
 //Semafaros
-sem_t cadeiras_epsera;
+sem_t cadeiras_espera;
 sem_t funcionario_levanta_pinos;
 sem_t semafaros_pistas[NumeroPistas];
 sem_t semafaros_pistas_pinos[NumeroPistas];
@@ -20,101 +23,152 @@ sem_t semafaros_pistas_pinos[NumeroPistas];
 pthread_cond_t espera_pista = PTHREAD_COND_INITIALIZER;
 
 //Locks
+//Lock para organizar os prints.
 pthread_mutex_t lock_print = PTHREAD_MUTEX_INITIALIZER;
+//Lock acessar a variável de condição para esperar uma pista.
 pthread_mutex_t lock_espera_pista = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t lock_acesso_dupla[NumeroPistas] = PTHREAD_MUTEX_INITIALIZER; 
+//Lock para uma dupla esperar a outra usar a pista para ela usar.
+pthread_mutex_t lock_acesso_dupla[NumeroPistas] = PTHREAD_MUTEX_INITIALIZER;
+ //Lock para acessar a variável pistasParaLevantarPinos.
+pthread_mutex_t lock_avisa_funcionario = PTHREAD_MUTEX_INITIALIZER;
+
+
 
 void* funcionario(void* arg) {
-    
-    pthread_mutex_lock(&lock_print);
-        std::cout <<"Funcionário iniciou seu expediente." <<std::endl;
-    pthread_mutex_unlock(&lock_print);
-    
-    while (true) {
-    sem_wait(&funcionario_levanta_pinos);
+    while (1) {
+        pthread_mutex_lock(&lock_print);
+            std::cout <<"Funcionário iniciou seu expediente." <<std::endl;
+        pthread_mutex_unlock(&lock_print);
+        
+        while (1) {
+        //Espera algum cliente sinalizar que sua pista precisa levantar os seus pinos
+        sem_wait(&funcionario_levanta_pinos);
+
+        //Percorre Todas as Pistas verificando qual precisa ter seus pinos levantados.
+        for (int i = 0; i < NumeroPistas; i++) {
+            pthread_mutex_lock(&lock_avisa_funcionario);
+            if(pistasParaLevantarPinos[i]){
+                //Retorna a condição da pista para Zero. Não precisa levantar os pinos.
+                pistasParaLevantarPinos[i] = 0;
+                //Incrementa em um o Semafaro da Pista para indicar que os pinos estão disponíveis.
+                sem_post(&semafaros_pistas_pinos[i]);
+
+                pthread_mutex_lock(&lock_print);
+                    std::cout << "Funcionário Levanta os pinos da pista de número: "<< i <<"."<< std::endl;
+                pthread_mutex_unlock(&lock_print);
+            }
+            pthread_mutex_unlock(&lock_avisa_funcionario);
+        }
+        
+        sleep(1);
+        }
     }
-    sleep(1);
 }
 
+/*
+Função que Simula a utilização da Pista de Boliche por um Cliete.
+Um cliete faz dois lançamentos e termina de usar a pista.
+O lançamento só pode ser feito caso os pinoss estejam levantados.
+Um Cliente deve chamar o Funcionário para levantar entre as suas jogadas.
+*/
 void utilizaPista(int id, int i){
     pthread_mutex_lock(&lock_acesso_dupla[i]);
 
+        //Espera ter pinos disponíveis.
         sem_wait(&semafaros_pistas_pinos[i]);
+        
         pthread_mutex_lock(&lock_print);
             std::cout << "Cliente de id: " << id << " faz primeiro lançamento na pista de número: "<< i <<"."<< std::endl;
         pthread_mutex_unlock(&lock_print);
+        
+        pthread_mutex_lock(&lock_avisa_funcionario);
+            //Post para avisar o funcionário que há pinos a serem levantados.
+            sem_post(&funcionario_levanta_pinos);
+            //Vaŕiavel que indicará qual pista o funcionário deverá incrementar o semáfaro para "Levantar os Pinos"
+            pistasParaLevantarPinos[i] = 1;
+        pthread_mutex_unlock(&lock_avisa_funcionario);
 
-        sem_post(&funcionario_levanta_pinos);
-
+        //Espera ter pinos disponíveis.
         sem_wait(&semafaros_pistas_pinos[i]);
+
         pthread_mutex_lock(&lock_print);
             std::cout << "Cliente de id: " << id << " faz segundo lançamento na pista de número: "<< i <<"."<< std::endl;
         pthread_mutex_unlock(&lock_print);
 
-        sem_post(&funcionario_levanta_pinos);
+        pthread_mutex_lock(&lock_avisa_funcionario);
+            //Post para avisar o funcionário que há pinos a serem levantados.
+            sem_post(&funcionario_levanta_pinos);
+            //Vaŕiavel que indicará qual pista o funcionário deverá incrementar o semáfaro para "Levantar os Pinos"
+            pistasParaLevantarPinos[i] = 1;
+        pthread_mutex_unlock(&lock_avisa_funcionario);
     
     pthread_mutex_unlock(&lock_acesso_dupla[i]);
-    
     sleep(2);
 }
 
-
 void* cliente(void* arg) {
-    int id = *((int *)arg);
-
-    if (sem_trywait(&cadeiras_epsera) == 0) {
-        pthread_mutex_lock(&lock_print);
-            std::cout << "Cliente de id: " << id << " conseguiu pegar uma cadeira" << std::endl;
-        pthread_mutex_unlock(&lock_print);
-            
-         bool conseguiuPista = false;
+    while (1) {
         
-        // Percorre todas as pistas tentando pegar uma
-        while (!conseguiuPista) {
-            for (int i = 0; i < NumeroPistas; i++) {
-                if (sem_trywait(&semafaros_pistas[i]) == 0) {
-                    conseguiuPista = true;
-                    sem_post(&cadeiras_epsera);
+        int id = *((int *)arg);
 
-                    pthread_mutex_lock(&lock_print);
-                        std::cout << "Cliente de id: " << id << " liberou uma cadeira de espera." << std::endl;
-                        std::cout << "Cliente de id: " << id << " está usando a pista " << i << std::endl;
-                    pthread_mutex_unlock(&lock_print);
+        //Cliente tenta pegar uma cadeira de espera
+        if (sem_trywait(&cadeiras_espera) == 0) {
+            pthread_mutex_lock(&lock_print);
+                std::cout << "Cliente de id: " << id << " conseguiu pegar uma cadeira" << std::endl;
+            pthread_mutex_unlock(&lock_print);
+                
+            bool conseguiuPista = false;
+            
+            //Percorre todas as pistas tentando pegar uma
+            while (!conseguiuPista) {
+                for (int i = 0; i < NumeroPistas; i++) {
 
-                    utilizaPista(id, i);
+                    //Cliente tenta pegar uma Pista
+                    if (sem_trywait(&semafaros_pistas[i]) == 0) {
 
-                    //Libere um espaço na pista e Avisa um cliente na fila de espera que o espaço está vago.
-                    sem_post(&semafaros_pistas[i]);
+                        //Cenário em que consegue pegar uma Pista
+                        conseguiuPista = true;
+                        sem_post(&cadeiras_espera);
+
+                        pthread_mutex_lock(&lock_print);
+                            std::cout << "Cliente de id: " << id << " liberou uma cadeira de espera." << std::endl;
+                            std::cout << "Cliente de id: " << id << " está usando a pista " << i << std::endl;
+                        pthread_mutex_unlock(&lock_print);
+
+                        utilizaPista(id, i);
+
+                        //Libere um espaço na pista e Avisa um cliente na fila de espera que o espaço está vago.
+                        sem_post(&semafaros_pistas[i]);
+                        pthread_mutex_lock(&lock_espera_pista);
+                            pthread_cond_signal(&espera_pista);
+                        pthread_mutex_unlock(&lock_espera_pista);
+
+                        pthread_mutex_lock(&lock_print);
+                            std::cout << "Cliente de id: " << id << " liberou a pista " << i << std::endl;
+                        pthread_mutex_unlock(&lock_print);
+                        break;
+                    }
+                }
+                if(!conseguiuPista){
+                    //Cenário em que Cliente não consegue a pista, irá eseprar até ser "avisado" por outro cliente
                     pthread_mutex_lock(&lock_espera_pista);
-                        pthread_cond_signal(&espera_pista);
+                        pthread_cond_wait(&espera_pista,&lock_espera_pista);
+                        
+                        pthread_mutex_lock(&lock_print);
+                            std::cout << "Cliente de id: " << id << " acordou "<< std::endl;
+                        pthread_mutex_unlock(&lock_print);
+                        
                     pthread_mutex_unlock(&lock_espera_pista);
-
-                    pthread_mutex_lock(&lock_print);
-                        std::cout << "Cliente de id: " << id << " liberou a pista " << i << std::endl;
-                    pthread_mutex_unlock(&lock_print);
-                    break;
                 }
             }
-            if(!conseguiuPista){
-                pthread_mutex_lock(&lock_espera_pista);
-                    pthread_cond_wait(&espera_pista,&lock_espera_pista);
-
-                    pthread_mutex_lock(&lock_print);
-                        std::cout << "Cliente de id: " << id << " acordou "<< std::endl;
-                    pthread_mutex_unlock(&lock_print);
-                    
-                pthread_mutex_unlock(&lock_espera_pista);
-            }
         }
-
-        pthread_mutex_lock(&lock_print);
-            std::cout <<"Cliente de id: " <<  id << " liberou uma cadeira" << std::endl;
-        pthread_mutex_unlock(&lock_print);
-    }
-    else{
-        pthread_mutex_lock(&lock_print);
-            std::cout <<"Cliente de id: " <<  id << " não conseguiu uma cadeira" << std::endl;
-        pthread_mutex_unlock(&lock_print);
+        else{
+            //Cénario Onde Cliente não consegue uma cadeira e vai embora.
+            pthread_mutex_lock(&lock_print);
+                std::cout <<"Cliente de id: " <<  id << " não conseguiu uma cadeira" << std::endl;
+            pthread_mutex_unlock(&lock_print);
+        }
+        sleep(4);
     }
 }
 
@@ -124,14 +178,14 @@ int main() {
 
     pthread_t thr_clientes[NumeroClientes], thr_funcionario;
 
-    // Inicialização dos semáforos para as pistas de boliche
+    //Inicialização dos semáforos para as pistas de boliche
     for (i = 0; i < NumeroPistas; i++) {
         sem_init(&(semafaros_pistas[i]), 0, 2);
         sem_init(&(semafaros_pistas_pinos[i]), 0, 1);
     }
     
-    // Inicialização do semáforo para as cadeiras de espera
-    sem_init(&cadeiras_epsera, 0, NumeroCadeiras);
+    //Inicialização do semáforo para as cadeiras de espera
+    sem_init(&cadeiras_espera, 0, NumeroCadeiras);
 
     //Criação das multiplas threads
     for (i = 0; i < NumeroClientes; i++) {
@@ -139,6 +193,7 @@ int main() {
         pthread_create(&thr_clientes[i], NULL, cliente, (void*) &id[i]);
     }
 
+    //Joins em cada threads gerada.
     pthread_create(&thr_funcionario, NULL, funcionario, NULL);
 
     for (i = 0; i < NumeroClientes; i++) {
